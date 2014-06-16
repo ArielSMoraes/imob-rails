5c7e132522532d0528bc19e13e1714b85655b3e4
= <hammerrhoads@gmail.com>
2014-06-15 22:06:01 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImobRails::Application.config.secret_key_base = 'f6521113bc724b0372cc1a75219625d7c1db5655fb45cc62c8a1c648f86cd45b6e57cafe529055a6a42e8613db661d5253a7e98031ebe4398f02ae9c869c9e74'
